5250130d527f61fab0a33754d04ab94c9f78e549
Dmitriy <svsdima1995@mail.ru>
2020-12-04 13:43:01 +0300

    Работаем с формами отправки данных

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import MainSlider from './modules/slider/slider-main';
 import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
+import Form from './modules/form';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -38,5 +39,7 @@ window.addEventListener('DOMContentLoaded', () => {
     player.init();
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
+
+    new Form('.form').init();
 
 });
